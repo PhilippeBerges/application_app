8a76c77f98c99ced85c00996f08c9631bae83bab
McBerges <mcberges31@ubuntu.(none)>
2013-02-28 03:02:42 -0800

    Fin partie 2

--- Gemfile
+++ Gemfile
@@ -7,7 +7,7 @@ group :production do
 end
 
 group :development, :test do
-gem 'sqlite3', '1.3.3'
+gem 'sqlite3', '1.3.4'
 end
 
 group :development do
@@ -17,4 +17,5 @@ end
 group :test do
 gem 'rspec-rails', '2.6.1'
 gem 'webrat', '0.7.1'
+gem 'spork','0.9.0.rc6'
 end
--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -1,19 +1,38 @@
 require 'spec_helper'
 
 describe PagesController do
-
+ render_views
   describe "GET 'home'" do
     it "should be successful" do
       get 'home'
       response.should be_success
     end
-  end
+   it "doit avoir le bon titre" do
+     get 'home'
+     response.should have_selector("title", :content => "App du tuto rails|Accueil")
+end
+end
 
   describe "GET 'contact'" do
     it "should be successful" do
       get 'contact'
       response.should be_success
     end
+  it "doit avoir le bon titre" do
+     get 'contact'
+     response.should have_selector("title", :content => "App du tuto rails|Contact")
+end  
+end
+
+  describe "GET 'about'" do
+    it "should be successful" do
+      get 'about'
+      response.should be_success
+    end
+  it "doit avoir le bon titre" do
+     get 'about'
+     response.should have_selector("title", :content => "App du tuto rails|About")
+end
   end
 
 end
